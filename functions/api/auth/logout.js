d0dc298929c17ad18eecc29cea442041fc9ce259
danielsherratt <danny.s13@live.com>
2025-09-12 23:36:04 +1200

    DELETE EVERYTHING

--- functions/api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-// functions/api/auth/logout.js
-export async function onRequestPost() {
-  const res = new Response(null, { status:204 });
-  res.headers.set('Set-Cookie', 'token=; HttpOnly; Secure; Path=/; SameSite=Strict; Max-Age=0');
-  return res;
-}
